2d511c52f9f464e52128eeb1b3ab2cf9a8f7ec85
Claire Hernandez <claire.hernandez@biblibre.com>
2019-06-10 16:01:27 +0200

    New queries
    - count biblios with some fields
    - all biblios
    - ready for next queries

--- Koha/Plugin/Com/BibLibre/TransitionBibliographique/cron/audit-tbiblio.pl
+++ Koha/Plugin/Com/BibLibre/TransitionBibliographique/cron/audit-tbiblio.pl
@@ -19,8 +19,10 @@ sub check_for_audit {
   say "\nGrille de catalogage default";
   &_get_marcframework_validation;
 
-
   say "\nCount records";
+  say &_query_database_for_count(&_get_count_biblios);
+
+  say "\nCount records with fields";
   say &_get_count_records_with_fields;
 }
 
@@ -44,29 +46,105 @@ sub _get_marcframework_validation {
 
 }
 
-#15:19 <jajm> et pour "requêter la base pour compter le nombre de notice avec tel ou telle valeur", le mieux c'est d'utiliser extractvalue à mon avis
+sub _query_database_for_count {
+  my (  $query ) = @_;
+  my $dbh = C4::Context->dbh;
+  my $count_sth = $dbh->prepare($query);
+  $count_sth->execute();
+  my ( $num_records ) = $count_sth->fetchrow;
+}
+
+sub _get_count_biblios {
+  my $query = q|
+          select count(*) from biblio;
+        |;
+}
+
+sub _get_count_ark_bnf {
+  # count O33a contient ark:/12148/
+}
+
+
+sub _get_count_ppn_id {
+  # count 009 ou O33a contient PPN ou sudoc.com
+}
+
+sub _get_count_others_033a {
+  # count O33a qui n'ont ni ark ni ppn
+}
+
+sub _get_count_aligned_biblios {
+  # count O33a contient un seul ark:/12148/ ou un ark et un PPN ou un PPN
+  # (notice "alignée de manière unique avec un réservoir national")
+}
+
 sub _get_count_records_with_fields {
-  #"SELECT biblionumber, ExtractValue(metadata,'count(//datafield[@tag="033"])') AS count033 FROM biblio_metadata HAVING count033 > 1|;"
+
+  my $marc_ref = {
+    '033a' => {
+      field => '033',
+      subfield => 'a',
+      count => undef
+    },
+    '181c' => {
+      field => '181',
+      subfield => 'c',
+      count => undef
+    },
+    '182c' => {
+      field => '182',
+      subfield => 'c',
+      count => undef
+    },
+    '183c' => {
+      field => '183',
+      subfield => 'c',
+      count => undef
+    },
+    '009' => {
+      field => '009',
+      subfield => undef,
+      count => undef
+    },
+    '219' => {
+      field => '219',
+      subfield => undef,
+      count => undef
+    }
+  };
 
   my $dbh = C4::Context->dbh;
-  my $count_sth = $dbh->prepare(
-      q|
-      SELECT biblionumber, ExtractValue(metadata,'count(//datafield[@tag="033"])') AS count033 FROM biblio_metadata HAVING count033 > 1
-      |
-  );
-#  my $count_sth = $dbh->prepare(
-#      q|
-#      SELECT COUNT(biblionumber)
-#      FROM biblio_metadata
-#      WHERE format='marcxml'
-#          AND (
-#              ExtractValue(metadata,'//datafield[@tag="033"]/subfield[@code="a"]')
-#              )
-#      |
-#  );17:16 <jajm> il manque quelque chose à ta condition, AND ExtractValue(...) [ != '' ? ]
+  my $query;
 
-  $count_sth->execute( C4::Context->preference('marcflavour') );
-  my ( $num_records ) = $count_sth->fetchrow;
+  foreach my $value (keys %$marc_ref) {
+
+    if (defined $marc_ref->{$value}->{"subfield"}) {
+      $query = q|
+          select count(*) as count from biblio_metadata
+          where ExtractValue(metadata, 'count(//datafield[@tag="#field#"]/subfield[@code="#subfield#"])')>0;
+        |;
+    } else {
+      $query = q|
+          select count(*) as count from biblio_metadata
+          where ExtractValue(metadata, 'count(//datafield[@tag="#field#"])')>0;
+        |;
+    }
+
+    my $query_to_run = $query =~ s/#field#/$marc_ref->{$value}->{"field"}/r;
+    $query_to_run = $query_to_run =~ s/#subfield#/$marc_ref->{$value}->{"subfield"}/r;
+
+    #print "querytorun:".$query_to_run;
+
+    my $count_sth = $dbh->prepare($query_to_run);
+    $count_sth->execute(  );
+    my ( $num_records ) = $count_sth->fetchrow;
+
+    print "$value : >>>".$num_records."\n";
+    $marc_ref->{$value}->{"count"} = $num_records;
+  }
+
+  warn Dumper $marc_ref;
+
 }
 
 &check_for_audit;
