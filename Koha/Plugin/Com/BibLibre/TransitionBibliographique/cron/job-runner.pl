4327c56a5990538f9e262e4040ff23a09196f450
Julian Maurice <julian.maurice@biblibre.com>
2019-09-05 14:26:55 +0200

    Prevent 2 instances of job-runner running at the same time

--- Koha/Plugin/Com/BibLibre/TransitionBibliographique/cron/job-runner.pl
+++ Koha/Plugin/Com/BibLibre/TransitionBibliographique/cron/job-runner.pl
@@ -2,10 +2,19 @@
 
 use Modern::Perl;
 
+use Fcntl ':flock';
+
 use C4::Context;
 use Koha::Plugins;
 
 use Koha::Plugin::Com::BibLibre::TransitionBibliographique;
+
+# Prevent two instances running at the same time
+open my $fh, '<', $0 or die "Cannot open $0 : $!";
+unless (flock $fh, LOCK_EX | LOCK_NB) {
+    say STDERR "job-runner is already running";
+    exit;
+}
 
 my $plugin = Koha::Plugin::Com::BibLibre::TransitionBibliographique->new({
     enable_plugins => 1,
